1b82143c7919c3b6a09de03541f7b402ef8e7e60
William Erickson <william@ericksonls.com>
2019-12-26 19:28:34 -0800

    v1.3.0.0 Update - Ammo drawing

--- Items/Weapons/ShieldGenerator.cs
+++ Items/Weapons/ShieldGenerator.cs
@@ -1,7 +1,9 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.UI.Chat;
 using Terraria.ModLoader;
 
 namespace ShieldMod.Items.Weapons
@@ -31,14 +33,14 @@ namespace ShieldMod.Items.Weapons
             item.UseSound = SoundID.Item13;
             item.shoot = mod.ProjectileType("Generator");
             item.shootSpeed = 14f;
-            //item.useAmmo = mod.ItemType("Deuterium");
+            //item.useAmmo = mod.ItemType("Deuterium"); //I don't use this to allow a different action on no ammo
             item.value = Item.sellPrice(1, 0, 0, 0);
         }
         public override bool CanUseItem(Player player)
         {
             if (player.HasItem(mod.ItemType("Deuterium")))
             {
-                player.ConsumeItem(mod.ItemType("Deuterium"));
+                player.ConsumeItem(mod.ItemType("Deuterium"));//Since the player has ammo, consume it, and set the projectile and mana to be used
                 item.shoot = mod.ProjectileType("GeneratorEX");
                 item.mana = 6;
             }
@@ -49,11 +51,33 @@ namespace ShieldMod.Items.Weapons
             }
             return base.CanUseItem(player);
         }
+        public int ammocount = 0;
+        public bool invopen = false;
+        public override void UpdateInventory(Player player)
+        {
+            invopen = Main.playerInventory;//Inventory open check
+            ammocount = 0;//Stop the loop from going to infinity and beyond
+            for (int j = 0; j < 58; j++)
+            {
+                if (player.inventory[j].type == mod.ItemType("Deuterium"))
+                {
+                    ammocount += player.inventory[j].stack;//Vanilla code to add up all of the ammo of a specific type in inventory
+                }
+            }
+        }
+        public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            //Because I don't use the normal "useAmmo" nothing appears in the inventory telling you how much ammo you have so we draw it manually
+            if (ammocount != 0 && !invopen)//Only draw the ammo count if you have ammo and the inventory isn't open
+            {
+                ChatManager.DrawColorCodedStringWithShadow(spriteBatch, Main.fontItemStack, ammocount.ToString(), position + new Vector2(-4f, 15f) * scale, drawColor, 0f, Vector2.Zero, new Vector2(scale * 0.8f), -1f, scale);
+            }
+        }
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.ChargedBlasterCannon);
-            //recipe.AddIngredient(mod.ItemType("Deuterium"), 10);
+            //recipe.AddIngredient(mod.ItemType("Deuterium"), 10);//old
             recipe.AddIngredient(ItemID.SoulofMight, 10);
             recipe.AddIngredient(ItemID.SpectreBar, 10);
             recipe.AddTile(TileID.MythrilAnvil);
